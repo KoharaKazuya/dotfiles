c08db7c74d2a826be75b7a4952f399a1a3097e04
KoharaKazuya <reflect.results@gmail.com>
2017-09-20 22:28:49 +0900

    git-hooks の警告メッセージをハイライトするように

--- git-hooks/pre-commit.d/detect-combine-chars.sh
+++ git-hooks/pre-commit.d/detect-combine-chars.sh
@@ -1,4 +1,4 @@
 # 全コミットファイル中、Unicode 合字 (濁点文字など) が含まれていないかチェックする
 if ! changed_files_by_commit | grep -E '^' >/dev/null 2>&1; then
-  changed_files_by_commit | xargs -I{} sh -c 'if file "{}" | grep -i text >/dev/null 2>&1; then cat "{}" | detect-combine-chars || printf "\e[33m%s\e[0m\n" "Unicode 合字が見つかりました: {}" >&2; fi'
+  changed_files_by_commit | xargs -I{} sh -c 'if file "{}" | grep -i text >/dev/null 2>&1; then cat "{}" | detect-combine-chars || printf "\e[33;1m%s\e[0m\n" "Unicode 合字が見つかりました: {}" >&2; fi'
 fi
