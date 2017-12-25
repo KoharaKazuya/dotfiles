fa2b982165c5063b7cbf50d0f1af647d595f8bb1
KoharaKazuya <reflect.results@gmail.com>
2017-12-25 19:34:34 +0900

    git-hooks detect-combine-chars のバグ修正

--- git-hooks/pre-commit.d/detect-combine-chars.sh
+++ git-hooks/pre-commit.d/detect-combine-chars.sh
@@ -1,4 +1,4 @@
 # 全コミットファイル中、Unicode 合字 (濁点文字など) が含まれていないかチェックする
-if ! changed_files_by_commit | grep -E '^' >/dev/null 2>&1; then
-  changed_files_by_commit | xargs -I{} sh -c 'if file "{}" | grep -i text >/dev/null 2>&1; then cat "{}" | detect-combine-chars || printf "\e[33;1m%s\e[0m\n" "Unicode 合字が見つかりました: {}" >&2; fi'
+if changed_files_by_commit | grep -E '.' >/dev/null 2>&1; then
+  changed_files_by_commit | xargs -I{} sh -c 'f="{}"; if file "$f" | grep -i text >/dev/null 2>&1; then cat "$f" | detect-combine-chars || (printf "\e[33;1m%s\e[0m\n" "Unicode 合字が見つかりました: $f" >&2; false); fi'
 fi
