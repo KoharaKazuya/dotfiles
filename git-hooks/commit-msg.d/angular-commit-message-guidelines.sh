538c87e0e0e57569ae0a09ece4c7428148d6e210
KoharaKazuya <reflect.results@gmail.com>
2019-03-16 15:06:39 +0900

    コミットメッセージガイドを追加

--- git-hooks/commit-msg.d/angular-commit-message-guidelines.sh
+++ git-hooks/commit-msg.d/angular-commit-message-guidelines.sh
@@ -23,5 +23,23 @@ if cat "$1" | head -n1 | grep -vE "$format_regexp" >/dev/null 2>&1; then
   echo ''
   echo '  コミットメッセージのチェックを無効にする場合は `git config private.angular-commit-message-guidelines false` を実行してください'
   echo ''
+  echo "$rev INFO $reset コミットメッセージガイド"
+  echo ''
+  echo '  ユーザーに提供する機能に影響のあるもの'
+  echo '    [Header]  feat:               新機能'
+  echo '    [Header]  fix:                不具合修正'
+  echo '    [Footter] BREAKING CHANGE:    互換性のない変更'
+  echo ''
+  echo '  ユーザーに影響のないアプリケーションコードの変更'
+  echo '    [Header]  perf:               パフォーマンス改善'
+  echo '    [Header]  refactor:           機能追加やバグ修正のないコードの変更'
+  echo '    [Header]  style:              コードの意味に影響のない変更'
+  echo '    [Header]  test:               不足テスト追加もしくは既存テストの修正'
+  echo ''
+  echo '  アプリケーションコードに関係しない変更'
+  echo '    [Header]  build:              ビルドシステムか外部依存に影響する変更'
+  echo '    [Header]  ci:                 CI 設定の変更'
+  echo '    [Header]  docs:               ドキュメントのみの変更'
+  echo ''
   exit 1
 fi
