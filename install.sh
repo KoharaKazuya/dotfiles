17cd65617b147fe468e32a4ca96e4f51a549b4aa
KoharaKazuya <reflect.results@gmail.com>
2020-01-28 20:45:31 +0900

    fix: インストールスクリプトがドットファイルが配置済みでなければ動かない不具合を修正する

--- install.sh
+++ install.sh
@@ -10,8 +10,6 @@ if [ ! -d $CONTAINER/dotfiles ]; then
   git clone https://github.com/KoharaKazuya/dotfiles.git
 fi
 
-zsh $CONTAINER/dotfiles/.zsh/.zim/zimfw.zsh install
-
 for dotfile in $(git ls-files | grep '^\.' | sed -e 's/\/.*//' | uniq)
 do
   if [ -e ~/$dotfile ]; then
@@ -22,6 +20,8 @@ do
     ln -s $CONTAINER/dotfiles/$dotfile ~/
   fi
 done
+
+zsh $CONTAINER/dotfiles/.zsh/.zim/zimfw.zsh install
 
 if type /bin/zsh > /dev/null 2>&1; then
   if [ "$SHELL" = /bin/zsh ]; then
