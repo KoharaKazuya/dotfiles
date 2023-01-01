f71a8f707e95373c735027f7fbb76ce4b8c98fa1
KoharaKazuya <reflect.results@gmail.com>
2023-01-01 21:44:23 +0900

    feat: DevContainer, Codespaces 環境対応

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# Development Containers, GitHub Codespaces 環境のみのセットアップ
+if [ "${REMOTE_CONTAINERS:-}" = true ] || [ "${CODESPACES:-}" = true ]; then
+  if cat /etc/os-release | grep PRETTY_NAME= | grep -i debian >/dev/null && builtin command -v apt-get /dev/null; then
+    # 日本語化
+    sudo sed -i -E 's/# (ja_JP.UTF-8)/\1/' /etc/locale.gen && sudo locale-gen && sudo update-locale LANG=ja_JP.UTF-8 # 日本語化
+    # よく使うコマンドをインストールする
+    curl -sSLf -o {/tmp,https://github.com/dandavison/delta/releases/download/0.15.1}/git-delta-musl_0.15.1_amd64.deb
+    sudo apt-get update && sudo apt-get install -y \
+      tig \
+      ripgrep \
+      /tmp/git-delta-musl_0.15.1_amd64.deb
+  fi
+fi
+
 CONTAINER=~/projects
 
 mkdir -p $CONTAINER
