181836b392b6d6ecd9952bcbbce33671ef965fdd
KoharaKazuya <reflect.results@gmail.com>
2023-01-01 21:51:48 +0900

    fix: install.sh の修正

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ set -e
 
 # Development Containers, GitHub Codespaces 環境のみのセットアップ
 if [ "${REMOTE_CONTAINERS:-}" = true ] || [ "${CODESPACES:-}" = true ]; then
-  if cat /etc/os-release | grep PRETTY_NAME= | grep -i debian >/dev/null && builtin command -v apt-get /dev/null; then
+  if cat /etc/os-release | grep PRETTY_NAME= | grep -i debian >/dev/null && type apt-get >/dev/null; then
     # 日本語化
     sudo sed -i -E 's/# (ja_JP.UTF-8)/\1/' /etc/locale.gen && sudo locale-gen && sudo update-locale LANG=ja_JP.UTF-8 # 日本語化
     # よく使うコマンドをインストールする
-    curl -sSLf -o {/tmp,https://github.com/dandavison/delta/releases/download/0.15.1}/git-delta-musl_0.15.1_amd64.deb
+    curl -sSLf -o /tmp/git-delta-musl_0.15.1_amd64.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb
     sudo apt-get update && sudo apt-get install -y \
       tig \
       ripgrep \
