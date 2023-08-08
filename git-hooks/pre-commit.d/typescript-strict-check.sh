97f74a087451bb2a0c4b9c7d29fac77285bf8663
KoharaKazuya <reflect.results@gmail.com>
2023-08-08 13:02:20 +0900

    feat: TypeScript の自動チェックでコンパイラのインストール状態をチェックするように

--- git-hooks/pre-commit.d/typescript-strict-check.sh
+++ git-hooks/pre-commit.d/typescript-strict-check.sh
@@ -3,6 +3,11 @@ root="$PWD"
 (echo .; changed_files_by_commit | sed -En -e ':loop' -e 's%/[^/]+$%%p;t loop' | sort | uniq) | while read line; do
   if cd "$root" && cd "$line" >/dev/null 2>&1; then
     if [ -f tsconfig.json ]; then
+      if ! npx --no-install tsc --help >/dev/null 2>&1; then
+        warn 'TypeScript がインストールされておらず tsc が実行できません'
+        show_ignore_variable
+        false
+      fi
       # 設定が strict: true でない場合のみ実行する (strict: true の場合は通常のビルド時にチェックされるため)
       if ! (npx --no-install tsc --showConfig | grep '"strict": true' >/dev/null); then
         if ! builtin command -v reviewdog >/dev/null 2>&1; then
