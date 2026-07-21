05c60ef2d9f07f6de39562edd5a8bed5e84ba404
KoharaKazuya <reflect.results@gmail.com>
2026-07-21 21:59:01 +0900

    feat(zsh): Android Studio がインストールされていればそれの Java を使用するようにパスを通す

--- .zsh/.zshrc.d/android-sdk.zsh
+++ .zsh/.zshrc.d/android-sdk.zsh
@@ -1,3 +1,8 @@
+if [ -d "/Applications/Android Studio.app" ]; then
+  export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+  export PATH="$JAVA_HOME/bin:$PATH"
+fi
+
 if [ -x "$HOME/Library/Android/sdk/platform-tools/adb" ]; then
   alias adb="$HOME/Library/Android/sdk/platform-tools/adb"
 fi
@@ -5,3 +10,4 @@ if [ -x "$HOME/Library/Android/sdk/tools/emulator" ]; then
   alias android-emulator-list="$HOME/Library/Android/sdk/tools/emulator -list-avds"
   alias android-emulator-run="$HOME/Library/Android/sdk/tools/emulator -avd"
 fi
+
